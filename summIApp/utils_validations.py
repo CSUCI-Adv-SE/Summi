1d9e41de42eb0d978c0d567417b0669a3f2d1946
Shahin Safarov <tungsten.rust@gmail.com>
2023-03-26 16:24:09 -0700

    util_validations security hardening added

--- summIApp/utils_validations.py
+++ summIApp/utils_validations.py
@@ -1,38 +1,25 @@
 from PIL import Image
 import logging
 from pathvalidate import sanitize_filename
-# regex library added
-import re
-logger = logging.getLogger("django")
+import traceback
+from io import BytesIO
 
+logger = logging.getLogger("django")
 
 def validate_file(file):
     try:
-        # Opening and closing files manually can cause errors if you forget to close the file.
-        # Using context managers (with statements) ensures that the file is automatically closed after the block of code finishes executing.
-        img = Image.open(file)
         with Image.open(file) as img:
-            return img.format.upper() in ["PNG", "JPG", "JPEG", "WEBP", "TIFF"]
+            img_format = img.format.upper()
+            return img_format in {"PNG", "JPG", "JPEG", "WEBP", "TIFF"}
     except Exception as e:
-        logger.error(str(e))
+        logger.error("Error validating file:", exc_info=True)
         return False
 
-
-def strip_html(name, max_length=100):
+def strip_html(name):
+    sanitized_filename = ""
     try:
-        # Remove leading and trailing whitespace
-        sanitized_filename = name.strip()
-
-        # Replace non-alphanumeric characters with underscores
-        sanitized_filename = re.sub(r"[^\w\s\-_]", "_", sanitized_filename)
-
-        # Limit the length of the filename
-        sanitized_filename = sanitized_filename[:max_length]
-
-        # Remove any remaining whitespace
-        sanitized_filename = sanitized_filename.replace(" ", "_")
-
+        sanitized_filename = sanitize_filename(name, replacement_text="")
         return sanitized_filename
     except Exception as e:
-        logger.error(str(e))
-        return ""
+        logger.error("Error sanitizing filename:", exc_info=True)
+    return sanitized_filename
