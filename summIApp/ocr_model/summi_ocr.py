72f416f93a39e4835873069a2344681a104d1ea3
Sarakhoshnoudrad <sara.khoshnoudrad890@myci.csuci.edu>
2023-03-27 11:53:01 -0700

    testing pytesseract

--- summIApp/ocr_model/summi_ocr.py
+++ summIApp/ocr_model/summi_ocr.py
@@ -1,28 +1,16 @@
-import easyocr
-import PIL
-import os
+import pytesseract as tess
+from PIL import Image
 import logging
+import os
 
+tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-logger = logging.getLogger("django")
-
-def recognize_text(uploaded_image):
+def recognized_text(uploaded_image):
     text = ""
     try:
-        # Load the image
-        image = PIL.Image.open(uploaded_image)
-
-        #prevent some path errors for other image formats
-        image_path = os.path.join(image.filename)
-
-        # Initialize EasyOCR reader
-        reader = easyocr.Reader(['en'])
-
-        # Use the reader to detect the text in the photo
-        result = reader.readtext(image_path)
-
-        # Extract the recognized text from the result and return it as a text (str)
-        text = ' '.join([r[1] for r in result])
+        img = Image.open(uploaded_image)
+        img_path = os.path.join(img.filename)
+        text = tess.image_to_string(img_path)
 
         return text.strip()
     except Exception as e:
