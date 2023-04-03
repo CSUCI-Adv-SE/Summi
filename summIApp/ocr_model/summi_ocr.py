9cb824e3a22609a460582712fbe2de999fddd33b
Prashanth <prashanthkumarreddy.busupal523@myci.csuci.edu>
2023-04-03 13:51:10 -0700

    hard coded path for tesseract

--- summIApp/ocr_model/summi_ocr.py
+++ summIApp/ocr_model/summi_ocr.py
@@ -2,14 +2,17 @@ import pytesseract as tess
 from PIL import Image
 import logging
 import os
+from summI.settings import DEBUG
 
-# tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
 def recognize_text(uploaded_image):
     text = ""
     try:
         img = Image.open(uploaded_image)
         img_path = os.path.join(img.filename)
+        if DEBUG:
+            tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+
         text = tess.image_to_string(img_path)
 
         return text.strip()
