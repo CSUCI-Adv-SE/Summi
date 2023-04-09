2c6fcc1aa59e988addca4d28641ac4024c17d31b
WNIA <wnia22@gmail.com>
2023-04-09 14:06:49 -0700

    [WIP] urlpatterns update

--- summIApp/urls.py
+++ summIApp/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("upload-file/", views.UserUploadedFilesView, name="UserUploadedFilesView"),
     # path("get-file/<uuid:image_id>", views.GetUserUploadedFileView, name="GetUserUploadedFileView"),
     path("summarise-text/",  views.GetSummarisedTextView, name="GetSummarisedTextView"),
+    path('register/', views.registerView, name = 'RegisterView'),
+    path('login/', views.loginView, name = 'LoginView'),
+    path('logout/', views.logoutView, name = 'LogoutView'),
 ]
