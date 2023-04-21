119ea13fb089c9dcfb4adf85b3e6b0b3baf91aeb
Prashanth <prashanthkumarreddy.busupal523@myci.csuci.edu>
2023-04-20 21:10:33 -0700

    added validations

--- summIApp/utils_validations.py
+++ summIApp/utils_validations.py
@@ -2,7 +2,7 @@ from PIL import Image
 import logging
 from pathvalidate import sanitize_filename
 import traceback
-
+import re
 
 logger = logging.getLogger("django")
 
@@ -24,3 +24,21 @@ def strip_html(name):
     except Exception as e:
         logger.error(traceback.format_exc())
     return sanitized_filename
+
+
+def is_email_valid(email):
+    try:
+        regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        return re.fullmatch(regex, email)
+    except Exception:
+        logger.error(traceback.format_exc())
+        return False
+
+
+def is_strong_password(password):
+    try:
+        regex = r"[A-Za-z0-9@#$%^&+=]{8,}"
+        return re.match(regex, password)
+    except Exception:
+        logger.error(traceback.format_exc())
+        return False
